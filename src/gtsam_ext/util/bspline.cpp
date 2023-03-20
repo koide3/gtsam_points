8e7d464f880f07f651490ca86d6cbdb38d795b7a
k.koide <k.koide@aist.go.jp>
2023-03-20 14:30:17 +0900

    fix bspline linear acceleration

--- src/gtsam_ext/util/bspline.cpp
+++ src/gtsam_ext/util/bspline.cpp
@@ -258,11 +258,9 @@ gtsam::Vector6_ bspline_imu(
 
   const double inv_knot_interval = 1.0 / knot_interval;
   const double inv_knot_interval2 = inv_knot_interval * inv_knot_interval;
-  // const gtsam::Vector3_ angular_vel = gtsam::unrotate(rot, inv_knot_interval * r_omega3);
 
   const gtsam::Vector3_ angular_vel = inv_knot_interval * r_omega3;
-  // const gtsam::Vector3_ angular_vel = gtsam::rotate(rot, inv_knot_interval * r_omega3);
-  const gtsam::Vector3_ linear_acc = gtsam::unrotate(rot, inv_knot_interval * t_omega3_ + gtsam::Vector3_(g));
+  const gtsam::Vector3_ linear_acc = gtsam::unrotate(rot, inv_knot_interval2 * t_omega3_ + gtsam::Vector3_(g));
 
   return gtsam_ext::concatenate(linear_acc, angular_vel);
 }
