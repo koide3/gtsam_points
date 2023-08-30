ee44089e63912ff8e099a0f4d83aead897c311d8
k.koide <k.koide@aist.go.jp>
2023-08-30 10:35:39 +0900

    gl_buffer_map

---
+++ include/gtsam_ext/cuda/gl_buffer_map.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstdlib>
+
+struct cudaGraphicsResource;
+
+namespace gtsam_ext {
+
+class GLBufferMap {
+public:
+  enum BufferMode { NONE, READ_ONLY, WRITE_ONLY };
+
+  GLBufferMap(size_t gl_id, BufferMode mode = NONE);
+  ~GLBufferMap();
+
+  size_t size() const { return buffer_size; }
+  void* data() { return d_buffer; }
+
+private:
+  cudaGraphicsResource* gl_resource;
+
+  size_t buffer_size;
+  void* d_buffer;
+};
+
+}  // namespace gtsam_ext
