aa5f03f8b0f9f216ed585f8e725a1fcb6df1f26e
k.koide <k.koide@aist.go.jp>
2025-01-12 22:49:02 +0900

    simply re-create gtsam::ImuFactor

--- include/gtsam_points/factors/reintegrated_imu_factor.hpp
+++ include/gtsam_points/factors/reintegrated_imu_factor.hpp
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2024  Kenji Koide (k.koide@aist.go.jp)
-
 #include <gtsam/navigation/ImuFactor.h>
 #include <gtsam/nonlinear/NonlinearFactor.h>
 
 namespace gtsam_points {
-
 /**
  * @brief IMU measurements to be re-integrated.
  *       This class stores IMU measurements along with the pre-integrated IMU measurements.
@@ -14,55 +12,39 @@ class ReintegratedImuMeasurements : public gtsam::PreintegratedImuMeasurements {
 public:
   friend class ReintegratedImuFactor;
 
-  ReintegratedImuMeasurements(
-    const boost::shared_ptr<gtsam::PreintegrationParams>& p,
-    const gtsam::imuBias::ConstantBias& biasHat = gtsam::imuBias::ConstantBias());
+  ReintegratedImuMeasurements(const boost::shared_ptr<gtsam::PreintegrationParams>& p, const gtsam::imuBias::ConstantBias& biasHat = gtsam::imuBias::ConstantBias());
   ~ReintegratedImuMeasurements() override;
 
   void resetIntegration() override;
   void integrateMeasurement(const gtsam::Vector3& measuredAcc, const gtsam::Vector3& measuredOmega, double dt) override;
 
-  const std::vector<gtsam::Vector7>& getImuData() const { return imu_data; }
+  const Eigen::Vector3d mean_acc() const;
+  const Eigen::Vector3d mean_gyro() const;
 
-private:
+public:
   std::vector<gtsam::Vector7> imu_data;  // [a, w, dt]
 };
-
 /**
  * @brief "Re"-integrated IMU factor (IMU factor without pre-integration).
  *        This factor re-integrates IMU measurements every linearization to better estimate the IMU bias.
  */
-class ReintegratedImuFactor
-: public gtsam::NoiseModelFactorN<gtsam::Pose3, gtsam::Vector3, gtsam::Pose3, gtsam::Vector3, gtsam::imuBias::ConstantBias, gtsam::Vector3> {
+class ReintegratedImuFactor : public gtsam::NonlinearFactor {
 public:
-  ReintegratedImuFactor(
-    gtsam::Key pose_i,
-    gtsam::Key vel_i,
-    gtsam::Key pose_j,
-    gtsam::Key vel_j,
-    gtsam::Key bias,
-    gtsam::Key gravity,
-    const ReintegratedImuMeasurements& imu_measurements);
+  ReintegratedImuFactor(gtsam::Key pose_i, gtsam::Key vel_i, gtsam::Key pose_j, gtsam::Key vel_j, gtsam::Key bias, const ReintegratedImuMeasurements& imu_measurements);
   ~ReintegratedImuFactor() override;
 
-  size_t dim() const override { return 15; }
+  size_t dim() const override { return 9; }
 
-  gtsam::Vector evaluateError(
-    const gtsam::Pose3& pose_i,
-    const gtsam::Vector3& vel_i,
-    const gtsam::Pose3& pose_j,
-    const gtsam::Vector3& vel_j,
-    const gtsam::imuBias::ConstantBias& bias,
-    const gtsam::Vector3& gravity,
-    boost::optional<gtsam::Matrix&> H_pose_i = boost::none,
-    boost::optional<gtsam::Matrix&> H_vel_i = boost::none,
-    boost::optional<gtsam::Matrix&> H_pose_j = boost::none,
-    boost::optional<gtsam::Matrix&> H_vel_j = boost::none,
-    boost::optional<gtsam::Matrix&> H_bias = boost::none,
-    boost::optional<gtsam::Matrix&> H_gravity = boost::none) const override;
+  boost::shared_ptr<gtsam::GaussianFactor> linearize(const gtsam::Values& values) const override;
+  double error(const gtsam::Values& values) const override;
+
+  const ReintegratedImuMeasurements& measurements() const { return imu_measurements; }
+
+private:
+  boost::shared_ptr<gtsam::ImuFactor> create_imu_factor(const gtsam::imuBias::ConstantBias& bias) const;
 
 private:
   const ReintegratedImuMeasurements imu_measurements;
+  mutable boost::shared_ptr<gtsam::ImuFactor> imu_factor;
 };
-
 }  // namespace gtsam_points
--- src/gtsam_points/factors/reintegrated_imu_factor.cpp
+++ src/gtsam_points/factors/reintegrated_imu_factor.cpp
@@ -1,10 +1,10 @@
-#include <gtsam_points/factors/reintegrated_imu_factor.hpp>
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2024  Kenji Koide (k.koide@aist.go.jp)
+#include <reintegrated_imu_factor.hpp>
 
 namespace gtsam_points {
 
-ReintegratedImuMeasurements::ReintegratedImuMeasurements(
-  const boost::shared_ptr<gtsam::PreintegrationParams>& p,
-  const gtsam::imuBias::ConstantBias& biasHat)
+ReintegratedImuMeasurements::ReintegratedImuMeasurements(const boost::shared_ptr<gtsam::PreintegrationParams>& p, const gtsam::imuBias::ConstantBias& biasHat)
 : gtsam::PreintegratedImuMeasurements(p, biasHat) {}
 
 ReintegratedImuMeasurements::~ReintegratedImuMeasurements() {}
@@ -16,10 +16,25 @@ void ReintegratedImuMeasurements::resetIntegration() {
 
 void ReintegratedImuMeasurements::integrateMeasurement(const gtsam::Vector3& measuredAcc, const gtsam::Vector3& measuredOmega, double dt) {
   gtsam::PreintegratedImuMeasurements::integrateMeasurement(measuredAcc, measuredOmega, dt);
-
   gtsam::Vector7 imu;
   imu << measuredAcc, measuredOmega, dt;
   imu_data.emplace_back(imu);
+}
+
+const Eigen::Vector3d ReintegratedImuMeasurements::mean_acc() const {
+  Eigen::Vector3d sum_acc = Eigen::Vector3d::Zero();
+  for (const auto& imu : imu_data) {
+    sum_acc += imu.block<3, 1>(0, 0);
+  }
+  return sum_acc / imu_data.size();
+}
+
+const Eigen::Vector3d ReintegratedImuMeasurements::mean_gyro() const {
+  Eigen::Vector3d sum_gyro = Eigen::Vector3d::Zero();
+  for (const auto& imu : imu_data) {
+    sum_gyro += imu.block<3, 1>(3, 0);
+  }
+  return sum_gyro / imu_data.size();
 }
 
 ReintegratedImuFactor::ReintegratedImuFactor(
@@ -28,44 +43,35 @@ ReintegratedImuFactor::ReintegratedImuFactor(
   gtsam::Key pose_j,
   gtsam::Key vel_j,
   gtsam::Key bias,
-  gtsam::Key gravity,
   const ReintegratedImuMeasurements& imu_measurements)
-: gtsam::NoiseModelFactorN<gtsam::Pose3, gtsam::Vector3, gtsam::Pose3, gtsam::Vector3, gtsam::imuBias::ConstantBias, gtsam::Vector3>(
-    gtsam::noiseModel::Isotropic::Sigma(15, 1.0),
-    pose_i,
-    vel_i,
-    pose_j,
-    vel_j,
-    bias,
-    gravity),
+: gtsam::NonlinearFactor(gtsam::KeyVector{pose_i, vel_i, pose_j, vel_j, bias}),
   imu_measurements(imu_measurements) {}
 
 ReintegratedImuFactor::~ReintegratedImuFactor() {}
 
-gtsam::Vector ReintegratedImuFactor::evaluateError(
-  const gtsam::Pose3& pose_i,
-  const gtsam::Vector3& vel_i,
-  const gtsam::Pose3& pose_j,
-  const gtsam::Vector3& vel_j,
-  const gtsam::imuBias::ConstantBias& bias,
-  const gtsam::Vector3& gravity,
-  boost::optional<gtsam::Matrix&> H_pose_i,
-  boost::optional<gtsam::Matrix&> H_vel_i,
-  boost::optional<gtsam::Matrix&> H_pose_j,
-  boost::optional<gtsam::Matrix&> H_vel_j,
-  boost::optional<gtsam::Matrix&> H_bias,
-  boost::optional<gtsam::Matrix&> H_gravity) const {
-  //
-  gtsam::NavState state_i(pose_i, vel_i);
-  for (const auto& imu : imu_measurements.getImuData()) {
-    const gtsam::Vector3 acc = bias.correctAccelerometer(imu.head<3>());
-    const gtsam::Vector3 omega = bias.correctGyroscope(imu.segment<3>(3));
-    const double dt = imu(6);
+boost::shared_ptr<gtsam::GaussianFactor> ReintegratedImuFactor::linearize(const gtsam::Values& values) const {
+  imu_factor = create_imu_factor(values.at<gtsam::imuBias::ConstantBias>(keys()[4]));
+  return imu_factor->linearize(values);
+}
 
-    gtsam::Matrix99 H_next_prev;
-    gtsam::Matrix93 H_next_acc, H_next_omega;
-    state_i = state_i.update(acc, omega, dt, H_next_prev, H_next_acc, H_next_omega);
+double ReintegratedImuFactor::error(const gtsam::Values& values) const {
+  if (!imu_factor) {
+    imu_factor = create_imu_factor(values.at<gtsam::imuBias::ConstantBias>(keys()[4]));
   }
+
+  return imu_factor->error(values);
+}
+
+boost::shared_ptr<gtsam::ImuFactor> ReintegratedImuFactor::create_imu_factor(const gtsam::imuBias::ConstantBias& bias) const {
+  gtsam::PreintegratedImuMeasurements pim(imu_measurements.params(), bias);
+  for (const auto& imu : imu_measurements.imu_data) {
+    const gtsam::Vector3 acc = imu.block<3, 1>(0, 0);
+    const gtsam::Vector3 gyro = imu.block<3, 1>(3, 0);
+    const double dt = imu[6];
+    pim.integrateMeasurement(acc, gyro, dt);
+  }
+
+  return gtsam::make_shared<gtsam::ImuFactor>(keys()[0], keys()[1], keys()[2], keys()[3], keys()[4], pim);
 }
 
 }  // namespace gtsam_points
