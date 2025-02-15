e70b53dce6604753d7ba3e8e62041dbfc23f6c9f
k.koide <k.koide@aist.go.jp>
2025-02-15 09:38:59 +0900

    add kdtreex.cpp

---
+++ src/gtsam_points/ann/kdtreex.cpp
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025  Kenji Koide (k.koide@aist.go.jp)
+
+#include <gtsam_points/ann/kdtreex.hpp>
+#include <gtsam_points/ann/impl/kdtreex_impl.hpp>
+
+namespace gtsam_points {
+
+template class KdTreeX<-1>;
+template class KdTreeX<4>;
+
+}  // namespace gtsam_points
