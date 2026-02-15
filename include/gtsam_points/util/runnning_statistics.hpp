38e11c7ef1927f4fedbb3060488c719d8a944317
k.koide <k.koide@aist.go.jp>
2026-02-15 20:42:03 +0900

    show warning

--- include/gtsam_points/util/runnning_statistics.hpp
+++ include/gtsam_points/util/runnning_statistics.hpp
@@ -3,4 +3,7 @@
 
 #pragma once
 
+// show compile warning
+#pragma message("Including gtsam_points/util/running_statistics.hpp is deprecated. Please include gtsam_points/util/running_statistics.hpp instead.")
+
 #include <gtsam_points/util/running_statistics.hpp>
